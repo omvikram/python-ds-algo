d34a23564328b0a19436a7fd99a05ee25881d0c3
omvikram <omvikram@hotmail.com>
2020-08-15 17:38:34 +0530

    file and graph

---
+++ graph.py
@@ -0,0 +1,27 @@
+class Graph:
+    
+    graph_dict={}
+    
+    def addEdge(self,node,neighbour):  
+        if node not in self.graph_dict:
+            self.graph_dict[node]=[neighbour]
+        else:
+            self.graph_dict[node].append(neighbour)
+            
+    def show_edges(self):
+        print("{")
+        for node in self.graph_dict:
+            for neighbour in self.graph_dict[node]:
+                print(node+"["+neighbour+"]")
+        print("}")
+
+g= Graph()
+g.addEdge('1', '2')
+g.addEdge('1', '3')
+g.addEdge('2', '3')
+g.addEdge('2', '1')
+g.addEdge('3', '1')
+g.addEdge('3', '2')
+g.addEdge('3', '4')
+g.addEdge('4', '3')
+g.show_edges()
