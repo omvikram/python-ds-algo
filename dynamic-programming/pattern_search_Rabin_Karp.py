c356c5f8659dc6e3234c340f26ff96cd8cf551c7
omvikram <omvikram@hotmail.com>
2020-08-23 15:24:41 +0530

    pattern search algos

---
+++ dynamic-programming/pattern_search_Rabin_Karp.py
@@ -0,0 +1,21 @@
+## Robin Karp Pattern Search works on the Hash Creation
+## It works on the theory that if the 2 strings are equal then their hash will also be equal
+## Time Complexity O(mn) where m and n are the length of the test and pattern respec. 
+
+txt = input("please enter the test string :")
+pat = input("please enter the pattern to search :")
+
+count = 0
+x = len(txt)
+y = len(pat)
+
+pat_hash = str(hash(pat))
+
+for i in range(0, x-y+1):
+    substr = txt[i:i+y]
+    substr_hash = str(hash(substr))
+    
+    if(pat_hash == substr_hash):
+        count = count + 1
+        
+print(count)
